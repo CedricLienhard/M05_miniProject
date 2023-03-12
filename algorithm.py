f6d5940f6d5d039a54d0dd504603c6c704d9cd34
Cedric Lienhard <lienhard.cedric@gmail.com>
2023-03-12 15:55:27 +0100

    test

--- algorithm.py
+++ algorithm.py
@@ -10,11 +10,29 @@ from sklearn.linear_model import LinearRegression
 
 
 def train_model(X_train, Y_train):
+    """
+    Optimizes the machine parameters to fit the input data
+    
+    
+    Parameters
+    ==========
+    X_train :   numpy.ndarray
+                The input data matrix. This must be an array with 3 dimensions or
+                an iterable containing 2 arrays with 2 dimensions each.  Each
+                correspond to the data for one of the two classes, every row
+                corresponds to one example of the data set, every column, one
+                different feature.
+    Returns
+    =======
+    machine : Machine
+        A trained machine.
+    """
+
 
 	# LR model
-	lin_model = LinearRegression()
-	trained_model = lin_model.fit(X_train, Y_train)
-	return trained_model
+    lin_model = LinearRegression()
+    trained_model = lin_model.fit(X_train, Y_train)
+    return trained_model
 
 def predict(X, trained_model):
     
