74a28964a7f592ac3390b3e8ff0dafb5045f3b66
Cedric Lienhard <lienhard.cedric@gmail.com>
2023-04-02 18:31:24 +0200

    update new package version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def load_requirements(f):
 
 setup(
     name="src_bostonHousingWineQuality",
-    version="0.5.8",
+    version="0.5.9",
     description="M05_miniProject",
     url="https://github.com/CedricLienhard/M05_miniProject",
     license="MIT",
